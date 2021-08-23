d13370e0f090dbbfdbce37c908c902d3675dcd1a
Austinia <kimdy90919@gmail.com>
2021-08-24 02:16:14 +0900

    Auth Done 2021/08/24/0216

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Routers from "components/Router";
 import { authService } from "Myfirebase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [Init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    })
+  }, [])
   return (
     <>
-      <Routers isLoggedIn={isLoggedIn} />
+      {Init ? <Routers isLoggedIn={isLoggedIn} /> : "초기화중입니다..."}
       <footer>&copy; Xwitter {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,85 @@
-import React from "react";
+import { authService, firebaseInstance } from "Myfirebase";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setnewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        // create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        // log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setnewAccount((prev) => !prev);
+  const onSocialClick = async(event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "twitter") {
+      provider = new firebaseInstance.auth.TwitterAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Sign Up" : "Log In"} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>{newAccount ? "Log in" : " Sign Up"}</span>
+      <div>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="twitter">
+          Continue with Twitter
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
