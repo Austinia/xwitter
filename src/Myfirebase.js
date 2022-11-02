2cdb9591eb4ed26643abd3625f13eed2d609c27a
Austinia <kimdy90919@gmail.com>
2022-11-02 16:45:26 +0900

    파이어베이스 교체 및 npm 업데이트

--- src/Myfirebase.js
+++ src/Myfirebase.js
@@ -1,7 +1,8 @@
 import firebase from "firebase/app";
-import "firebase/auth"
-import "firebase/firestore"
-import "firebase/storage"
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -9,10 +10,12 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+firebase.getAnalytics(app);
 
 export const firebaseInstance = firebase;
 
